4e5b60908f0365e130a09024737a0b0301131a10
Juliana Iasi <juaiasi@gmail.com>
2022-06-03 09:38:11 -0300

    configs to connect with user app

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_SUJ2pOaxs', // e.g. us-east-1_SUJ2pOaxs
+        userPoolClientId: '5lpncdhjhh9tt2h61621lbncj2', // e.g. 5lpncdhjhh9tt2h61621lbncj2
+        region: 'us-east-1' // e.g. us-east-1
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
