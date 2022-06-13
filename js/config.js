9ccea6f6d2093e2dbe3a5d0f8959d583a6895c9f
juaiasi <juaiasi@gmail.com>
2022-06-13 09:57:10 -0300

    api gateway

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@ window._config = {
         userPoolClientId: '6pmik9nd2td8kdfu11pqdlbuqk', // e.g. 5lpncdhjhh9tt2h61621lbncj2
         region: 'us-east-1' // e.g. us-east-1
     },
-    api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-    }
+    api: { 
+        invokeUrl: 'https://uchmqkcw6l.execute-api.sa-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod, 
+    } 
 };
