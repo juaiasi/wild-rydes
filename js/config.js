63a29c6b91b6c891b661454cf1f9295b874a2f14
juaiasi <juaiasi@gmail.com>
2022-06-13 09:19:41 -0300

    cognito user pool

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_SUJ2pOaxs', // e.g. us-east-1_SUJ2pOaxs
-        userPoolClientId: '5lpncdhjhh9tt2h61621lbncj2', // e.g. 5lpncdhjhh9tt2h61621lbncj2
+        userPoolId: 'sa-east-1_sAq2VnG6b', // e.g. us-east-1_SUJ2pOaxs
+        userPoolClientId: '6pmik9nd2td8kdfu11pqdlbuqk', // e.g. 5lpncdhjhh9tt2h61621lbncj2
         region: 'us-east-1' // e.g. us-east-1
     },
     api: {
